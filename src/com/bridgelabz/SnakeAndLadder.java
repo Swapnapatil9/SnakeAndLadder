5352d259064e68143f02155f73c34c81665c47b3
“Swapnapatil9” <swapnapatil629@gmail.com>
2022-12-25 16:21:18 +0530

    Added:The Player rolls the die to get a number

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,10 +3,14 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     static final int STARTING_POSITION = 0;
     static final int WINNING_POSITION = 100;
+    static final int playersPosition= STARTING_POSITION;
+    static int diceRoll() {
+        int x = (int) (1+ Math.random() * 6);
+        return x;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder program");
-        int playersPosition= STARTING_POSITION;
         System.out.println("Starting position of player is :"+playersPosition);
-
+        System.out.println(diceRoll());
     }
 }
