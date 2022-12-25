cb3a0e4310ecc4f6ca2cbeb7f5435834904bc8e2
“Swapnapatil9” <swapnapatil629@gmail.com>
2022-12-25 20:03:13 +0530

    Added:Play the game with 2 Player.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -6,48 +6,87 @@ public class SnakeAndLadder {
     static final int NO_PLAY = 0;
     static final int IS_SNAKE = 1;
     static final int IS_LADDER = 2;
+    static final int playerPosition=STARTING_POSITION;
+
+    static int diceCount = 0;
 
     static int diceRoll() {
-        int x = (int) (1+ Math.random() * 6);
+        int x = (int) (1 + Math.random() * 6);
         return x;
     }
+
     static int getOption() {
         int x = (int) (Math.random() * 3);
         return x;
     }
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snake and Ladder program");
 
-        int playerPosition = STARTING_POSITION;
+    static int getToss() {
+        int x = (int) (Math.random() * 2);
+        return x;
+    }
 
-        int diceCount=0;
-
-        while (playerPosition < WINNING_POSITION){
-            int roll=diceRoll();
-            diceCount++;
-            System.out.println(roll);
+    static int playGame(int playerPosition) {
+        int roll = diceRoll();
+        diceCount++;
+        System.out.println(roll);
 
         int option = getOption();
-
         switch (option) {
             case IS_SNAKE:
                 System.out.println("Snake");
                 playerPosition -= roll;
-                if (playerPosition<STARTING_POSITION)
-                    playerPosition=STARTING_POSITION;
+                if (playerPosition < STARTING_POSITION)
+                    playerPosition = STARTING_POSITION;
                 break;
             case IS_LADDER:
                 System.out.println("Ladder");
                 playerPosition += roll;
-                if(playerPosition > WINNING_POSITION)
+                if (playerPosition > WINNING_POSITION)
                     playerPosition -= roll;
                 break;
             default:
                 System.out.println("No play");
+
         }
-            System.out.println("Dice count:" +diceCount);
-            System.out.println("  ,Player position:" +playerPosition);
+        System.out.print("Dice count : " + diceCount);
+        System.out.println("  , Player position : " + playerPosition);
+
+        if (option == IS_LADDER) {
+            playGame(playerPosition);
         }
-        System.out.println("Total Dice Count:" +diceCount);
+
+        return playerPosition;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder program");
+
+        int player1Position = STARTING_POSITION;
+        int player2Position = STARTING_POSITION;
+
+        int toss = getToss();
+
+        while (player1Position < WINNING_POSITION && player2Position < WINNING_POSITION) {
+
+            if (toss == 0) {
+                System.out.println("PLAYER 1 IS ROLLING");
+                player1Position = playGame(player1Position);
+                toss = 1;
+            } else {
+                System.out.println("PLAYER 2 IS ROLLING");
+                player2Position = playGame(player2Position);
+                toss = 0;
+            }
+            if (player1Position == WINNING_POSITION) {
+                System.out.println("Player-1 is winner");
+                break;
+            }
+            if (player2Position == WINNING_POSITION) {
+                System.out.println("Player-2 is winner");
+                break;
+            }
+
+
+        }
     }
 }
